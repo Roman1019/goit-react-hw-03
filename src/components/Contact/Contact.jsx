5d1e6d3f56f16bce4e9a5c8a3b9d7d688b97cd82
Roman1019 <142348028+Roman1019@users.noreply.github.com>
2025-03-10 11:11:13 +0200

    thirth commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,37 +4,31 @@ import css from "./Contact.module.css";
 
 export default function ContactList({ contacts, onDelete }) {
   return (
-    <div className={css.contactForm}>
-      <ul className={css.contactUl}>
-        {contacts.map((contact) => (
-          <li className={css.contactItem} key={contact.id}>
-            <div>
-              <div className={css.contactHeadDiv}>
-                <div className={css.contactHeadIconDiv}>
-                  <FaUser />
-                </div>
+    <>
+      <div>
+        <div className={css.contactHeadDiv}>
+          <div className={css.contactHeadIconDiv}>
+            <FaUser />
+          </div>
 
-                <h3 className={css.contactHead}>{contact.name}</h3>
-              </div>
-              <div className={css.contactParDiv}>
-                <div className={css.contactHeadIconDiv}>
-                  <FaPhoneAlt />
-                </div>
-                <p className={css.contactPar}>{contact.number}</p>
-              </div>
-            </div>
-            <div className={css.contactButtonDiv}>
-              <button
-                className={css.contactButton}
-                onClick={() => onDelete(contact.id)}
-                type="button"
-              >
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <h3 className={css.contactHead}>{contacts.name}</h3>
+        </div>
+        <div className={css.contactParDiv}>
+          <div className={css.contactHeadIconDiv}>
+            <FaPhoneAlt />
+          </div>
+          <p className={css.contactPar}>{contacts.number}</p>
+        </div>
+      </div>
+      <div className={css.contactButtonDiv}>
+        <button
+          className={css.contactButton}
+          onClick={() => onDelete(contacts.id)}
+          type="button"
+        >
+          Delete
+        </button>
+      </div>
+    </>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,16 @@
 import Contact from "../Contact/Contact.jsx";
+import css from "./ContactList.module.css";
 
 export default function ContactList({ contacts, onDelete }) {
   return (
     <div>
-      <Contact contacts={contacts} onDelete={onDelete} />
+      <ul className={css.contactUl}>
+        {contacts.map((contact) => (
+          <li className={css.contactItem} key={contact.id}>
+            <Contact contacts={contact} onDelete={onDelete} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
